947e666e285f482707598137f8d38319f38547e2
Henry Stern <108289013+henrystern@users.noreply.github.com>
2025-03-08 21:53:28 -0500

    use new favicon

--- src/SternPlot/config.py
+++ src/SternPlot/config.py
@@ -4,4 +4,4 @@ from pathlib import Path
 
 # Paths
 PROJ_ROOT = Path(__file__).resolve().parents[0]
-FAVICON_PATH = PROJ_ROOT / "static/favicon-96x96.png"
+FAVICON_PATH = PROJ_ROOT / "static/favicon.png"
