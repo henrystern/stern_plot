38de68724275f38cbd554424fb879916a1f4f6a5
Henry Stern <108289013+henrystern@users.noreply.github.com>
2025-07-09 09:13:49 -0400

    fix no favicon when installed as package

--- src/stern_plot/config.py
+++ src/stern_plot/config.py
@@ -1,7 +1,14 @@
 """Configuration parameters and paths."""
 
+from io import BytesIO
 from pathlib import Path
+
+import requests
 
 # Paths
 PROJ_ROOT = Path(__file__).resolve().parents[0]
 FAVICON_PATH = PROJ_ROOT / "static/favicon.png"
+
+if not FAVICON_PATH.exists():
+    logo_url = "https://hstern.ca/static/assets/web-app-manifest-512x512.png"
+    FAVICON_PATH = BytesIO(requests.get(logo_url).content)
